315a60c6311cdee61d8e19f3c38af9eba9eaa661
Szymon Tyrka <szy.tyrka@gmail.com>
2021-05-31 09:51:11 +0200

    lab 11

--- app/business/business.container.js
+++ app/business/business.container.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import userManager from './user.manager';
-
+import postManager from './post.manager';
 
 function getter(manager, request) {
   return function () {
@@ -10,5 +10,7 @@ function getter(manager, request) {
 }
 
 export default {
-    getUserManager: getter(userManager),
+	getUserManager: getter(userManager),
+	getPostManager: getter(postManager)
 };
+
